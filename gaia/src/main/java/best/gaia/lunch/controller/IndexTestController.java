e6ce11683f737516ddf6ddf30bb1e697e2d76b44
maskan19 <maskan_19@naver.com>
2021-05-12 15:47:49 +0900

    page setting
    page setting

---
+++ gaia/src/main/java/best/gaia/lunch/controller/IndexTestController.java
@@ -0,0 +1,36 @@
+package best.gaia.lunch.controller;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+
+import best.gaia.lunch.service.LunchService;
+import oracle.jdbc.proxy.annotation.Methods;
+
+@Controller
+public class IndexTestController {
+	
+	@Inject
+	private LunchService service;
+	@Inject
+	private WebApplicationContext container;
+	private ServletContext application;
+	@PostConstruct
+	public void init() {
+		application = container.getServletContext();
+	}
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping(value="/admin/index", method = RequestMethod.GET)
+	public String loginPage() {
+		return "admin/index";
+	}
+	
+}
