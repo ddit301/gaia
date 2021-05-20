8737a68d0eacb2d50b4b7dc043afdae5771303cd
maskan19 <maskan_19@naver.com>
2021-05-20 17:20:41 +0900

    prepare for security

--- gaia/src/main/java/best/gaia/vo/MemberVO.java
+++ gaia/src/main/java/best/gaia/vo/MemberVO.java
@@ -1,5 +1,12 @@
 package best.gaia.vo;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +14,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberVO {
+public class MemberVO implements UserDetails, Serializable{
+	
 	private int mem_no;             //회원 번호
 	private String mem_id;          //회원 아이디(이메일)
 	private String mem_pass;        //회원 비밀번호
@@ -20,4 +28,71 @@ public class MemberVO {
 	private String mem_bio;         //회원 자기소개 내용
 	private String mem_working_city;//회원 근무 도시
 	private String mem_status;      //회원 상태
+	private List<GrantedAuthority> authorities;
+	private boolean enabled = mem_status.equals("ACTIVE")? true: false;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return mem_pass;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return mem_id;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MemberVO other = (MemberVO) obj;
+		if (mem_no != other.mem_no)
+			return false;
+		if (mem_pass == null) {
+			if (other.mem_pass != null)
+				return false;
+		} else if (!mem_pass.equals(other.mem_pass))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mem_no;
+		result = prime * result + ((mem_pass == null) ? 0 : mem_pass.hashCode());
+		return result;
+	}
+	
 }
