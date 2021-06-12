5ee23241a4a937baa7eef6b27682f7b273ae5bcd
maskan19 <maskan_19@naver.com>
2021-06-12 17:44:04 +0900

    resource

--- gaia/src/main/java/best/gaia/monitoring/controller/MonitoringController.java
+++ gaia/src/main/java/best/gaia/monitoring/controller/MonitoringController.java
@@ -1,20 +1,23 @@
 package best.gaia.monitoring.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import best.gaia.monitoring.service.ClassResourceMonitoringService;
 import best.gaia.monitoring.service.EnvironmentResourceMonitoringService;
 import best.gaia.monitoring.service.MemoryMonitoringService;
 import best.gaia.monitoring.service.ThreadMonitoringService;
 
-@Controller
+@RestController
 @RequestMapping("admin/monitoring")
 public class MonitoringController {
 	@Inject
@@ -26,23 +29,33 @@ public class MonitoringController {
 	@Inject
 	private ThreadMonitoringService tmService;
 	
-	@RequestMapping
-	public String view(Model model) throws IOException {
-		StringBuffer buffer = new StringBuffer();
-		model.addAttribute("info", buffer);
-		erService.osInfo(buffer);
-		erService.runtimeInfo(buffer);
-		erService.diskSpaceCheck(buffer);
-		miService.memorySummary(buffer);
-		miService.gabageCollectionInfo(buffer);
-		miService.memoryPoolInfo(buffer);
-		crService.compilerCheck(buffer);
-		crService.classLoaderCheck(buffer);
-		tmService.threadSummary(buffer);
-		return "admin/monitoring";
+	@GetMapping(value="info")
+	public Map<String, Object> view(Model model) throws IOException {
+		Map<String, Object> result = new LinkedHashMap<>();
+		
+		Map<String, Object> Environment_Resource = new LinkedHashMap<>();
+		Environment_Resource.put("OS_Information", erService.osInfo());
+		Environment_Resource.put("JVM_Information", erService.runtimeInfo());
+		Environment_Resource.put("Disk_Information", erService.diskSpaceCheck());
+		result.put("Environment_Resource", Environment_Resource);
+		
+		Map<String, Object> Memory = new LinkedHashMap<>();
+		Memory.put("Memory_Usage_Summary", miService.memorySummary());
+		Memory.put("Garbage_Collection_Information", miService.gabageCollectionInfo());
+		Memory.put("Memory_Pool_Usage_Information", miService.memoryPoolInfo());
+		result.put("Memory", Memory);
+		
+		Map<String, Object> Class_Resource = new LinkedHashMap<>();
+		Class_Resource.put("Memory_Usage_Summary", miService.memorySummary());
+		Class_Resource.put("Garbage_Collection_Information", miService.gabageCollectionInfo());
+		result.put("Class_Resource", Class_Resource);
+		
+//		crService.compilerCheck(buffer);
+//		crService.classLoaderCheck(buffer);
+//		tmService.threadSummary(buffer);
+		return result;
 	}
-	@RequestMapping(value="/threadDump", produces="text/plain;charset=UTF-8")
-	@ResponseBody
+	@RequestMapping(value="/threadDump")
 	public StringBuffer threadDump(Model model) throws IOException {
 		StringBuffer buffer = new StringBuffer();
 		model.addAttribute("info", buffer);
