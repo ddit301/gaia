b0b3d86758276a202f33a23e976053b1c0ebd8cf
KrGil <kanjjang89@naver.com>
2021-06-21 12:27:15 +0900

    캘린더 해당 이슈혹은 마일스톤 클릭 시 alert창 띄우기
    마일스톤과 이슈 open close 된 것들만 보이기로 선택 했을 때 isShowAll parameter 넘기기.
    전부 보이는 페이지를 제외하고
    open-> close로 바꾸면 view에서 보이지 않게.

--- gaia/webapp/resources/js/calendar.js
+++ gaia/webapp/resources/js/calendar.js
@@ -16,47 +16,119 @@ const issueMilestoneInfoForCalendar = function(){
 		url : getContextPath()+"/restapi/project/calendar",
 		method : 'get',
 		success : function(res) {
+			scrollUp();
 			console.log(res);
-			let arr=addCalendarArray(res);
+			// "0", "1", 공백  으로 status를 표시할 수 있다.
+			let arr=addCalendarArray(res, );
+			
 			console.log(arr);
-			calendar(arr);
+			// "0", "1", 공백  으로 status를 표시할 수 있다.
+			calendar(arr, );
 		},
-		async : false,
-		error : function(xhr) {
-			console.log(xhr);
-			// 해당 404 는 뜨면 안되는 에러지만, 충분한 테스팅 후 아래 alert 모두 적절한 예외 처리 필요
-			if(xhr.status == '404'){
-				alert("실패");				
-			}else{
-				alert("status : " + xhr.status);
-			}
+		async : false
+		, error : function(xhr, error, msg) {
+			ajaxError(xhr, error, msg)
 		},
 		dataType : 'json'
 	})
 }
-const addCalendarArray = function(res){
+// 2021-01-11 
+const YYYYMMDD = function(time){
+	time = moment(time).format('YYYY-MM-DD');
+	return time; 
+}
+// 클릭 시 해당 메뉴를 닫을지 열것인지 알러트
+const closeOrOpenAlert = function(arg, isShowAll){
+	let status = "CLOSE";
+	arg.event.extendedProps.status == 0 ? status : status="OPEN"; 
+	  
+	swalWithBootstrapButtons = Swal.mixin({
+		customClass: {
+			confirmButton: 'btn btn-success',
+			cancelButton: 'btn btn-danger'
+		},
+		buttonsStyling: false
+	})
+	swalWithBootstrapButtons.fire({
+		title: status,
+		text: "해당 "+ arg.event.extendedProps.menu + "를 "+status+" 하시겠습니까?",
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonText: '네',
+		cancelButtonText: '아니오',
+		reverseButtons: false
+	}).then((result) => {
+		if (result.isConfirmed) {
+			// update
+			
+			// false면 값이존재한다.  true는 undefined
+			CheckNullUndefined(isShowAll) ?  null : arg.event.remove();
+			swal.success();
+		}
+	})
+}
+// arr에 모든 마일스톤과 이슈 담기.
+const printChart = function(name, arr){
+	let currentTime = new Date();
+	currentTime = moment(currentTime).format('YYYY-MM-DD');
+	let menu = "ISSUE";
+	let status = "";
+	if("issue_sid" in name){
+		if(!CheckNullUndefined(name.issue_end_date)){
+			arr.push({
+				menu: menu,
+				sid: name.issue_sid,
+				status: name.issue_status,
+	  			title: name.issue_title,
+	  			start: YYYYMMDD(name.issue_start_date) ? YYYYMMDD(name.issue_start_date) : currentTime,
+				end : YYYYMMDD(name.issue_end_date)
+	  		})
+		}
+	}else{
+		if(!CheckNullUndefined(name.milest_end_date)){
+			menu="MILESTONE"
+	  		arr.push({
+				menu: menu,
+				sid: name.milest_sid,
+				status: name.milest_status,
+	  			title: name.milest_title,
+	  			start: name.milest_start_date
+	  		})
+	  		arr.push({
+				menu: menu,
+				sid: name.milest_sid,
+				status: name.milest_status,
+				borderColor: "#d83737",
+	  			title: name.milest_title,
+	  			start: name.milest_end_date
+	  		})
+		}
+	}
+}
+// 조건 부여하기.
+const addCalendarArray = function(res, status){
 	let arr = [];
-	$.each(res.milestoneList, function(i, milestone){
-		  if(!CheckNullUndefined(milestone.milest_end_date)){
-	  		  arr.push({
-	  			  title: milestone.milest_cont,
-	  			  start: milestone.milest_start_date
-	  		  })
-		  }
-	  })
+	// open, close, all
+	$.each(res.milestoneList, function(i, milest){
+		status ? (milest.milest_status==status ? printChart(milest, arr,status) : null) :  printChart(milest, arr,status);
+	})
+	$.each(res.issueList, function(i, issue){
+		status ? (issue.issue_status==status ? printChart(issue, arr,status) : null) :  printChart(issue, arr,status);
+	})
 	return arr;
 }
 
-const calendar = function(arr){
+const calendar = function(arr, isShowAll){
+	let currentTime = new Date();
+	currentTime = moment(currentTime).format('YYYY-MM-DD');
     var calendarEl = document.getElementById('calendar');
-
     var calendar = new FullCalendar.Calendar(calendarEl, {
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      initialDate: '2021-09-12',
+      initialDate: currentTime,
       navLinks: true, // can click day/week names to navigate views
       selectable: true,
       selectMirror: true,
@@ -73,14 +145,28 @@ const calendar = function(arr){
         }
         calendar.unselect()
       },
-      eventClick: function(arg) {
-        if (confirm('Are you sure you want to delete this event?')) {
-          arg.event.remove()
-        }
+      eventClick: function(arg, status) {
+		console.log(arg)
+		if(arg.event.extendedProps.status == 0){
+			closeOrOpenAlert(arg);
+		}else if(arg.event.extendedProps.status == 1){
+			closeOrOpenAlert(arg);
+		}
+		// status 가 공백이면 (모두 보여주기)
+		if(CheckNullUndefined(isShowAll)){
+			if(arg.event.extendedProps.status == 0){
+        		closeOrOpenAlert(arg, status);
+			}else if(ar.event.extendedProps.status == 1){
+				if (confirm('Are you sure you want to open this event?')) {
+				// todo update
+				}
+			}
+		}
       },
       editable: true,
       dayMaxEvents: true, // allow "more" link when too many events
       events: arr
+
 //       [
 //         {
 //           title: 'All Day Event',
@@ -140,4 +226,6 @@ const calendar = function(arr){
     });
     calendar.render();
 }
+
+
 
